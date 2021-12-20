4a7a248163d7c45387988687028e5601bd887439
bagamanshin <bagamanshin@mail.ru>
2021-12-20 17:07:00 +0700

    isntall ts-node

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
+import express, { Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get('*', (req: typeof express.Request, res: typeof express.Response) => {
+app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '/dist/', 'index.html'));
 });
 
